c3b8a6665d61322be62af2a0dffdce10ab92bcdd
Ashley M Laing <ashleym.laing@gmail.com>
2018-05-30 16:02:07 -0400

    Create slack-tutorial.js

---
+++ slack-tutorial.js
@@ -0,0 +1,44 @@
+var Webtask = require('webtask-tools')
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+
+var app = express()
+
+app.use(bodyParser.json())
+app.get('/', function (req, res) {
+  
+  if (req.query.token !== req.webtaskContext.secrets.verification_token) {
+    res.sendStatus(401);
+    return;
+  }
+  var headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + req.webtaskContext.secrets.token
+  }
+  
+  var options = {
+    uri: "https://slack.com/api/chat.postMessage",
+    method: 'POST',
+    headers: headers,
+    json: true,
+    body: {
+    text: `Hello, you wrote: "${req.query.text}"`,
+    // endpoint: req.query.text,
+    channel: req.query.channel_id
+    }
+  }
+  
+  // let ending = options.endpoint;
+  
+  // for(let i = 0; i < ending.length; i++){
+  //   if(ending[i] == " "){
+  //     ending[i] = "+";
+  //   }
+  // }
+  
+  request(options)
+  res.sendStatus(200)
+})
+
+module.exports = Webtask.fromExpress(app)
